7c0179867868da4d2a769d902760f6286faa54cb
Hong_EuiMin <luca@Hong-EuiMinui-MacBookPro.local>
2025-03-27 16:14:34 +0900

    버그 수정: FutureBuilder의 future를fetchQuiz() -> futureQuiz

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,18 +9,17 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late Future<void> futureQuiz;
+  late Future<Map<String, dynamic>> futureQuiz;
 
-  //초기화
+  // 초기화
   @override
   void initState() {
     super.initState();
     futureQuiz = fetchQuiz();
   }
 
-  //새로운 문제
+  // 새로운 문제
   void loadNewQuiz() {
-    //setState()를 호출하면 화면이 다시 그려짐
     setState(() {
       futureQuiz = fetchQuiz();
     });
@@ -29,37 +28,38 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //앱바
+      // 앱바
       appBar: AppBar(title: Text('랜덤 퀴즈')),
 
-      //바디
+      // 바디
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            //퀴즈 출력
+            // 퀴즈 출력
             FutureBuilder(
-              future: fetchQuiz(),
+              future: futureQuiz,
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
                   // 변수 선언
-                  final correctAnswer =
-                      snapshot.data!['results'][0]['correct_answer'];
-                  final incorrectAnswers =
-                      snapshot.data!['results'][0]['incorrect_answers'];
-                  //정답이랑 오답이랑 합친 List
+                  final resultData =
+                      snapshot.data!['results'][0]; //사용하기 쉽게 변수에 저장
+
+                  final correctAnswer = resultData['correct_answer'];
+                  final incorrectAnswers = resultData['incorrect_answers'];
+                  // 정답이랑 오답이랑 합친 List
                   final answers = [
-                    correctAnswer, //얘는 정답이어서 리스트가 아닌 문자열
-                    ...incorrectAnswers, //Spread Operator
+                    correctAnswer, // 얘는 정답이어서 리스트가 아닌 문자열
+                    ...incorrectAnswers, // Spread Operator
                   ];
-                  answers.shuffle(); //섞기
+                  answers.shuffle(); // 섞기
 
                   return Column(
                     children: [
-                      //질문 출력
+                      // 질문 출력
                       Text(
-                        "${snapshot.data!['results'][0]['question']}",
+                        "${resultData['question']}",
                         textAlign: TextAlign.center,
                         style: const TextStyle(
                           fontWeight: FontWeight.bold,
@@ -68,7 +68,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                       const SizedBox(height: 20),
 
-                      //답안 출력
+                      // 답안 출력
                       for (var answer in answers)
                         Row(
                           children: [
@@ -88,15 +88,19 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                       const SizedBox(height: 30),
 
-                      //새로운 문제 버튼
-                      ElevatedButton(
-                        onPressed: loadNewQuiz,
-                        child: const Text('새로운 문제'),
+                      // 새로운 문제 버튼
+                      IgnorePointer(
+                        ignoring:
+                            snapshot.connectionState == ConnectionState.waiting,
+                        child: ElevatedButton(
+                          onPressed: loadNewQuiz,
+                          child: const Text('새로운 문제'),
+                        ),
                       ),
                     ],
                   );
                 } else if (snapshot.hasError) {
-                  return Text('$snapshot.error');
+                  return Text('${snapshot.error}');
                 } else {
                   return const CircularProgressIndicator();
                 }
@@ -108,21 +112,3 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
-
-// {
-//   "response_code": 0,
-//   "results": [
-//     {
-//       "type": "multiple",
-//       "difficulty": "easy",
-//       "category": "Science &amp; Nature",
-//       "question": "How many planets make up our Solar System?",
-//       "correct_answer": "8",
-//       "incorrect_answers": [
-//         "7",
-//         "9",
-//         "6"
-//       ]
-//     }
-//   ]
-// }
